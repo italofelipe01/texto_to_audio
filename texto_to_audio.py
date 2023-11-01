db311fc8290d43ab8eab2efb79a2d7bcd89450c0
Ítalo Felipe Lira de Morais <italo_felipe01@hotmail.com>
2023-11-01 15:17:04 -0300

    commit_teste

--- texto_to_audio.py
+++ texto_to_audio.py
@@ -1,5 +1,8 @@
 from gtts import gTTS
+import os
+from datetime import datetime
 
+time = datetime.now
 text = input("Insira sua mensagem a ser disponibilizada em audio por favor: ")
 tts = gTTS(text, lang='pt', tld='com.br')
 tts.save('audio.mp3')
