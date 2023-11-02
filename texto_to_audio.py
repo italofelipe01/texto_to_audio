5ae12af2ad5336b34e15736afc6a6cb10dbc3c2f
Ítalo Felipe Lira de Morais <italo_felipe01@hotmail.com>
2023-11-02 14:32:38 -0300

    texto_to_audio-changes-02-11-2023

--- texto_to_audio.py
+++ texto_to_audio.py
@@ -6,3 +6,5 @@ time = datetime.now
 text = input("Insira sua mensagem a ser disponibilizada em audio por favor: ")
 tts = gTTS(text, lang='pt', tld='com.br')
 tts.save('audio.mp3')
+
+os.system("ffplay -autoexit audio.mp3")
